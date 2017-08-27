1a7845a639332f914c21fbb8e0a516a1ffab82a8
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-08-26 20:25:13 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,61 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
+function FetchURL($URL, $Method = 'GET', $Arguments = false,$Authorization = false,$UserAgent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13',$OtherHeaders = false){
+  
+  if($URL==''){
+    return false;
+  }
+  
+  //Make sure method is uppercase
+  $Method=strtoupper($Method);
+  
+  //Initialize Arguments array if null
+  if($Arguments==false){
+    $Arguments=array();
+  }
+  //Set up cURL  
+  $cURL = curl_init();
+  curl_setopt($cURL,CURLOPT_URL, $URL);
+  
+  //Maybe add arguments into POSTFIELDS
+  if($Method=='POST'){
+    curl_setopt($cURL,CURLOPT_POST, count($Arguments));
+    $URLArguments = http_build_query($Arguments);
+    curl_setopt($cURL,CURLOPT_POSTFIELDS, $URLArguments);
+  }
+  
+  //Maybe pass authorization
+  if($Authorization){
+    curl_setopt($cURL,CURLOPT_HTTPHEADER, array(
+      'Authorization: Bearer '.$Token
+    ));
+  }
+  
+  if($OtherHeaders){
+    curl_setopt($cURL,CURLOPT_HTTPHEADER, $OtherHeaders);
+  }
+  
+  if($Method=='PUT'){
+    curl_setopt($cURL,CURLOPT_RETURNTRANSFER, false);
+  }else{
+    curl_setopt($cURL,CURLOPT_RETURNTRANSFER, true);
+  }
+  
+  curl_setopt($cURL,CURLOPT_USERAGENT,$UserAgent);
+  
+  
+  //Run cURL and close it
+  $Data = curl_exec($cURL);
+  curl_close($cURL);
+  
+  //Note: This function returns the data as it is received; it is not parsed.
+  
+  return $Data;
+}
+
 function gitGlobalHash(){
-  $Hash = file_get_contents('https://api.github.com/repos/cjtrowbridge/vps-home/git/refs/heads/master');
+  $Hash = FetchURL('https://api.github.com/repos/cjtrowbridge/vps-home/git/refs/heads/master');
   $Hash = json_decode($Hash,true);
   if($Hash==false){return false;}
   if(!(isset($Hash['object']))){return false;}
