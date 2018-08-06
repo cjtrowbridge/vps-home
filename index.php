8b78a5a2954a5ab3a52e96064326e348b74d729f
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-08-05 18:52:48 -0700

    Update index.php

--- index.php
+++ index.php
@@ -123,7 +123,7 @@ if(isset($_GET['fetch'])){
 			echo '<!--Opening Backups Dir: /var/www/backups-->'.PHP_EOL;
 			if($handle = opendir('/var/www/backups')){
 				while (false !== ($dir = readdir($handle))){
-					if($dir != "." && $dir != ".."){
+					if($dir != "." && $dir != ".." && $dir != ".sync"){
 						echo shell_exec('du -sh /var/www/backups/'.$dir.'/*');
 					}else{
 						echo "<!--Skipping ".$dir."-->".PHP_EOL;
