c3696664fc1868334b82955664d2ed326ab3add1
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2024-08-25 22:54:33 -0700

    Update fix_permissions.sh

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
+sudo chown -R www-data:user /var/www/
+sudo chmod -R 775 /var/www/
 
-chown www-data:www-data /var/www/index.php -fR  &2>1 > /dev/null
-chown www-data:www-data /var/www/webs -fR  &2>1 > /dev/null
-chmod 750 /var/www/webs -fR &2>1 > /dev/null
-find /var/www/webs/ -type d -exec chmod 755 -f {} +
-find /var/www/webs/ -type f -exec chmod 644 -f {} +
-
-#chown -R rslsync:www-data /var/www/backups
-#chmod -R 775 /var/www/backups
-
-#chown -R rslsync:www-data /var/www/webs/memes.cjtrowbridge.com
-#chmod -R 775 /var/www/webs/memes.cjtrowbridge.com
+sudo cp ~/vps-home/index.html /var/www/index.html
+sudo cp ~/vps-home/fix_permissions.sh ~/fix_permissions.sh
