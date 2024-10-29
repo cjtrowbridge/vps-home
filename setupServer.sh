a9f1cc2f8c8d33967a9399766226e9a36a079704
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2024-10-28 17:42:39 -0700

    Update setupServer.sh

--- setupServer.sh
+++ setupServer.sh
@@ -6,16 +6,6 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 
-# Prompt for TightVNC username and password
-read -p "Enter the username for TightVNC: " VNC_USER
-read -sp "Enter the password for TightVNC: " VNC_PASS
-echo
-
-# Prompt for Samba username and password
-read -p "Enter the username for Samba: " SAMBA_USER
-read -sp "Enter the password for Samba: " SAMBA_PASS
-echo
-
 # Prompt for .htpasswd username and password
 read -p "Enter a long random text segment for the htpasswd username: " HTUSER
 read -sp "Enter a long random text segment for the htpasswd password: " HTPASS
@@ -26,7 +16,7 @@ wget http://packages.azlux.fr/debian/pool/main/b/bashtop/bashtop_0.9.25_all.deb
 sudo dpkg -i bashtop_0.9.25_all.deb
 
 # Common packages for all servers
-COMMON_PACKAGES="nload htop apache2 fail2ban tightvncserver git git-lfs apt-transport-https unattended-upgrades ufw logrotate samba"
+COMMON_PACKAGES="nload htop apache2 fail2ban git git-lfs apt-transport-https unattended-upgrades ufw logrotate"
 
 # PHP packages for all except www-static
 PHP_PACKAGES="php8.0 php8.0-bcmath php8.0-bz2 php8.0-intl php8.0-gd php8.0-mbstring php8.0-mysql php8.0-zip php8.0-xml php8.0-curl php8.0-sqlite3"
@@ -40,21 +30,15 @@ sudo apt-get update
 # Install common packages
 sudo apt-get install -y $COMMON_PACKAGES
 
-# Install PHP and related packages on all but www-static
-if [[ "$HOSTNAME" != "www-static" ]]; then
-    sudo apt-get install lsb-release apt-transport-https ca-certificates
-    sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-    sudo echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
-    sudo apt-get update
-    sudo apt-get install -y $PHP_PACKAGES
-fi
+sudo apt-get install lsb-release apt-transport-https ca-certificates
+sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+sudo echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+sudo apt-get update
+sudo apt-get install -y $PHP_PACKAGES
+sudo apt-get install -y $SQL_PACKAGES
 
-# Install MariaDB and phpMyAdmin on the SQL server
-if [[ "$HOSTNAME" == "www-sql" ]]; then
-    sudo apt-get install -y $SQL_PACKAGES
-    # Run mysql_secure_installation
-    sudo mysql_secure_installation
-fi
+# Run mysql_secure_installation
+sudo mysql_secure_installation
 
 # Ensure SSH is enabled and started
 sudo systemctl enable ssh && sudo systemctl start ssh
@@ -65,10 +49,8 @@ sudo systemctl enable apache2 && sudo systemctl start apache2
 # Disable any active sites
 sudo a2dissite * && sudo service apache2 restart
 
-# Optional: Enable and start mariadb on SQL server
-if [[ "$HOSTNAME" == "www-sql" ]]; then
-    sudo systemctl enable mariadb && sudo systemctl start mariadb
-fi
+#Enable and start mariadb on SQL server
+sudo systemctl enable mariadb && sudo systemctl start mariadb
 
 # Enable and configure unattended upgrades
 sudo dpkg-reconfigure --priority=low unattended-upgrades
@@ -77,7 +59,6 @@ sudo dpkg-reconfigure --priority=low unattended-upgrades
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow OpenSSH
-sudo ufw allow Samba
 sudo ufw allow 'Apache Full'
 sudo ufw --force enable
 
@@ -97,29 +78,6 @@ sudo systemctl restart apache2
 
 # Ensure /var/www exists
 sudo mkdir -p /var/www
-
-# Configure Samba
-# Create Samba user
-sudo smbpasswd -a $SAMBA_USER <<< "$SAMBA_PASS"$'\n'"$SAMBA_PASS"
-sudo smbpasswd -e $SAMBA_USER
-
-# Add Samba configuration
-cat <<EOT | sudo tee -a /etc/samba/smb.conf
-
-[www]
-   comment = nas
-   path = /var/www
-   guest ok = no
-   browseable = yes
-   create mask = 0777
-   directory mask = 0777
-   writable = yes
-   read only = no
-
-EOT
-
-# Restart Samba service
-sudo systemctl restart smbd && sudo systemctl restart nmbd
 
 # Install and configure Certbot for SSL
 sudo apt install snapd
@@ -158,5 +116,3 @@ EOT
 # Enable the site and restart Apache
 sudo a2ensite 000-default.conf
 sudo systemctl restart apache2
-
-echo "Setup complete for $HOSTNAME"
